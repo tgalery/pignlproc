9d4a108466f0b2dd07e6c64141a1cf7fa56fced8
Chris Hokamp <chris.hokamp@gmail.com>
2012-06-22 12:28:05 -0500

    updated Get_Counts_Lucene.java to use Spotlight stoplist and English_Analyzer

--- src/main/java/pignlproc/index/Get_Counts_Lucene.java
+++ src/main/java/pignlproc/index/Get_Counts_Lucene.java
@@ -4,10 +4,15 @@ import java.io.*;
 import java.util.*;
 
 import com.bea.xml.stream.StaticAllocator;
-import org.apache.commons.io.FileUtils;
 
+import com.google.common.io.Files;
+import com.google.common.collect.Sets;
+import com.google.common.base.Charsets;
+
+import org.apache.commons.lang.CharSet;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.tokenattributes.TermAttribute;
 import org.apache.lucene.util.Version;
@@ -30,18 +35,30 @@ import org.apache.pig.impl.logicalLayer.schema.Schema.FieldSchema;
 
 public class Get_Counts_Lucene extends EvalFunc<DataBag> {
 
-    public static final String ENGLISH_STOPWORDS_PATH = "opennlp/stopwords.en.list";
+    public static final String ENGLISH_STOPWORDS_PATH = "stopwords.en.list";
 
+    protected final HashSet<String> stopwords;
     TupleFactory tupleFactory = TupleFactory.getInstance();
     BagFactory bagFactory = BagFactory.getInstance();
     //Hard-coded for the Lucene standard analyzer because this is unnecessary for this implementation
     static final String field = "paragraph";
 
+    public Get_Counts_Lucene () throws IOException {
+        ClassLoader loader = getClass().getClassLoader();
+
+        String path = ENGLISH_STOPWORDS_PATH;
+        InputStream in = loader.getResourceAsStream(path);
+        File file = new File(path);
+
+        stopwords = Sets.newHashSet(Files.readLines(file, Charsets.UTF_8));
+
+    }
+
     @Override
     public DataBag exec(Tuple input) throws IOException {
 
         //TODO: pass in the dbpedia-spotlight stopword list
-        Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_30);
+        Analyzer analyzer = new EnglishAnalyzer(Version.LUCENE_36, stopwords);
 
         DataBag out = bagFactory.newDefaultBag();
         Object t0 = input.get(0);
@@ -63,7 +80,7 @@ public class Get_Counts_Lucene extends EvalFunc<DataBag> {
         {
             //there should be only one item in each tuple
             String text = (String) it.next().get(0);
-            //TODO: change to Lucene tokenizer
+
             TokenStream stream = analyzer.tokenStream(field, new StringReader(text));
 
             try {
