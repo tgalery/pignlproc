4959fac25bc20a3e1108a4b2b4d86868b393aa66
Chris Hokamp <chris.hokamp@gmail.com>
2012-09-18 03:52:24 -0500

    bug fix for distributed cache

--- src/main/java/pignlproc/index/GetCountsLucene.java
+++ src/main/java/pignlproc/index/GetCountsLucene.java
@@ -44,8 +44,8 @@ public class GetCountsLucene extends EvalFunc<DataBag> {
     //Hard-coded for the Lucene standard analyzer because this is unnecessary for this implementation
     static final String field = "paragraph";
 
-    private String stoplist_path; //the path to the stoplist
-    private String stoplist_name; //the name of the stoplist
+    private String stoplist_path = null; //the path to the stoplist
+    private String stoplist_name = null; //the name of the stoplist
     private HashSet<String> stopset = null;
     protected Analyzer analyzer;
     private TokenStream stream = null;
