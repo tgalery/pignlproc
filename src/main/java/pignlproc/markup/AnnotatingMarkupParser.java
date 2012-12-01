466e35103fc336742601ef298030ed3999754cd1
jodaiber <jodaiber@gmx.de>
2012-12-01 17:27:27 +0100

    Adding language-specific Redirect patterns from DBpedia.

--- src/main/java/pignlproc/markup/AnnotatingMarkupParser.java
+++ src/main/java/pignlproc/markup/AnnotatingMarkupParser.java
@@ -28,9 +28,9 @@ import java.util.regex.Pattern;
  * Parse mediawiki markup to strip the formatting info and extract a simple text
  * version suitable for NLP along with header, paragraph and link position
  * annotations.
- * 
+ *
  * Use the {@code #convert(String)} and {@code #getWikiLinks()} methods.
- * 
+ *
  * Due to the constraints imposed by the {@code ITextConverter} /
  * {@code WikiModel} API, this class is not thread safe: only one instance
  * should be run by thread.
@@ -71,14 +71,156 @@ public class AnnotatingMarkupParser implements ITextConverter {
     protected Pattern redirectPattern;
 
 
-    //TODO: fix for compatibility with other languages
     private static Map<String, Pattern> getRedirectPatterns() {
         Map<String, Pattern> m = new HashMap<String, Pattern>();
-        m.put("en", Pattern.compile("^#REDIRECT \\[\\[([^\\]]*)\\]\\]"));
+        m.put("af", Pattern.compile("^(?:#AANSTUUR|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ar", Pattern.compile("^(?:#تحويل|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("arz", Pattern.compile("^(?:#تحويل|#تحويل|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("be-tarask", Pattern.compile("^(?:#перанакіраваньне|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("bg", Pattern.compile("^(?:#пренасочване|#виж|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("br", Pattern.compile("^(?:#ADKAS|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("bs", Pattern.compile("^(?:#PREUSMJERI|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("cs", Pattern.compile("^(?:#REDIRECT|#PŘESMĚRUJ) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("cu", Pattern.compile("^(?:#ПРѢНАПРАВЛЄНИѤ|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("cy", Pattern.compile("^(?:#ail-cyfeirio|#ailgyfeirio|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
         m.put("de", Pattern.compile("^(?:#WEITERLEITUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
-        m.put("fr", Pattern.compile("^(?:#REDIRECTION|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("el", Pattern.compile("^(?:#ΑΝΑΚΑΤΕΥΘΥΝΣΗ|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("en", Pattern.compile("^(?:#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("eo", Pattern.compile("^(?:#ALIDIREKTU|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
         m.put("es", Pattern.compile("^(?:#REDIRECCIÓN|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("et", Pattern.compile("^(?:#suuna|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("eu", Pattern.compile("^(?:#BIRZUZENDU|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("fa", Pattern.compile("^(?:#تغییرمسیر|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("fi", Pattern.compile("^(?:#OHJAUS|#UUDELLEENOHJAUS|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("fr", Pattern.compile("^(?:#REDIRECTION|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ga", Pattern.compile("^(?:#redirect|#athsheoladh) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("gl", Pattern.compile("^(?:#REDIRECCIÓN|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("he", Pattern.compile("^(?:#הפניה|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("hr", Pattern.compile("^(?:#PREUSMJERI|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("hu", Pattern.compile("^(?:#ÁTIRÁNYÍTÁS|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("hy", Pattern.compile("^(?:#REDIRECT|#ՎԵՐԱՀՂՈՒՄ) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("id", Pattern.compile("^(?:#ALIH|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("is", Pattern.compile("^(?:#tilvísun|#TILVÍSUN|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ja", Pattern.compile("^(?:#転送|#リダイレクト|＃転送|＃リダイレクト|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ka", Pattern.compile("^(?:#REDIRECT|#გადამისამართება) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("kk-arab", Pattern.compile("^(?:#REDIRECT|#ايداۋ) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("kk-cyrl", Pattern.compile("^(?:#REDIRECT|#АЙДАУ) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("kk-latn", Pattern.compile("^(?:#REDIRECT|#AÝDAW) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("km", Pattern.compile("^(?:#បញ្ជូនបន្ត|#ប្ដូរទីតាំងទៅ #ប្តូរទីតាំងទៅ|#ប្ដូរទីតាំង|#ប្តូរទីតាំង|#ប្ដូរចំណងជើង|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ko", Pattern.compile("^(?:#넘겨주기|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ksh", Pattern.compile("^(?:#ÖMLEIDUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("lt", Pattern.compile("^(?:#PERADRESAVIMAS|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("mk", Pattern.compile("^(?:#пренасочување|#види|#Пренасочување|#ПРЕНАСОЧУВАЊЕ|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ml", Pattern.compile("^(?:#REDIRECT|#തിരിച്ചുവിടുക|തിരിച്ചുവിടല്‍) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("mr", Pattern.compile("^(?:#पुनर्निर्देशन|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("mt", Pattern.compile("^(?:#RINDIRIZZA|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("mwl", Pattern.compile("^(?:#ANCAMINAR|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("nds", Pattern.compile("^(?:#redirect|#wiederleiden) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("nds-nl", Pattern.compile("^(?:#DEURVERWIEZING|#DOORVERWIJZING|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("nl", Pattern.compile("^(?:#DOORVERWIJZING|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("nn", Pattern.compile("^(?:#omdiriger|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("oc", Pattern.compile("^(?:#REDIRECCION|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("pl", Pattern.compile("^(?:#PATRZ|#PRZEKIERUJ|#TAM|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
         m.put("pt", Pattern.compile("^(?:#REDIRECIONAMENTO|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ro", Pattern.compile("^(?:#REDIRECTEAZA|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ru", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("sa", Pattern.compile("^(?:#पुनर्निदेशन|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("sd", Pattern.compile("^(?:#چوريو|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("si", Pattern.compile("^(?:#යළියොමුව|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("sk", Pattern.compile("^(?:#presmeruj|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("sq", Pattern.compile("^(?:#RIDREJTO|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("srn", Pattern.compile("^(?:#STIR|#DOORVERWIJZING|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("sr-ec", Pattern.compile("^(?:#Преусмери|#redirect|#преусмери|#ПРЕУСМЕРИ) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("sr-el", Pattern.compile("^(?:#Preusmeri|#redirect|#preusmeri|#PREUSMERI) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("sv", Pattern.compile("^(?:#OMDIRIGERING|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ta", Pattern.compile("^(?:#வழிமாற்று|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("te", Pattern.compile("^(?:#దారిమార్పు|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("th", Pattern.compile("^(?:#เปลี่ยนทาง|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("tr", Pattern.compile("^(?:#YÖNLENDİRME|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("tt-latn", Pattern.compile("^(?:#yünältü|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("uk", Pattern.compile("^(?:#ПЕРЕНАПРАВЛЕННЯ|#ПЕРЕНАПР|#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("vi", Pattern.compile("^(?:#đổi|#đổi|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("vro", Pattern.compile("^(?:#saadaq|#suuna|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("yi", Pattern.compile("^(?:#ווייטערפירן|#הפניה|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ab", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ace", Pattern.compile("^(?:#ALIH|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("aln", Pattern.compile("^(?:#RIDREJTO|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("als", Pattern.compile("^(?:#WEITERLEITUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("an", Pattern.compile("^(?:#REDIRECCIÓN|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("arn", Pattern.compile("^(?:#REDIRECCIÓN|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("av", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ay", Pattern.compile("^(?:#REDIRECCIÓN|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ba", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("bar", Pattern.compile("^(?:#WEITERLEITUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("bat-smg", Pattern.compile("^(?:#PERADRESAVIMAS|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("bcc", Pattern.compile("^(?:#تغییرمسیر|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("be-x-old", Pattern.compile("^(?:#перанакіраваньне|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("bm", Pattern.compile("^(?:#REDIRECTION|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("bqi", Pattern.compile("^(?:#تغییرمسیر|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("bug", Pattern.compile("^(?:#ALIH|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("cbk-zam", Pattern.compile("^(?:#REDIRECCIÓN|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ce", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("crh-cyrl", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("cv", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("de-at", Pattern.compile("^(?:#WEITERLEITUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("de-ch", Pattern.compile("^(?:#WEITERLEITUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("de-formal", Pattern.compile("^(?:#WEITERLEITUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("dsb", Pattern.compile("^(?:#WEITERLEITUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ff", Pattern.compile("^(?:#REDIRECTION|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("fiu-vro", Pattern.compile("^(?:#saadaq|#suuna|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("frp", Pattern.compile("^(?:#REDIRECTION|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("gag", Pattern.compile("^(?:#YÖNLENDİRME|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("glk", Pattern.compile("^(?:#تغییرمسیر|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("gn", Pattern.compile("^(?:#REDIRECCIÓN|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("gsw", Pattern.compile("^(?:#WEITERLEITUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("hsb", Pattern.compile("^(?:#WEITERLEITUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ht", Pattern.compile("^(?:#REDIRECTION|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("inh", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("jv", Pattern.compile("^(?:#ALIH|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("kaa", Pattern.compile("^(?:#REDIRECT|#AÝDAW) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("kk", Pattern.compile("^(?:#REDIRECT|#АЙДАУ) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("kk-cn", Pattern.compile("^(?:#REDIRECT|#ايداۋ) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("kk-kz", Pattern.compile("^(?:#REDIRECT|#АЙДАУ) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("kk-tr", Pattern.compile("^(?:#REDIRECT|#AÝDAW) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("kv", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("lad", Pattern.compile("^(?:#REDIRECCIÓN|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("lb", Pattern.compile("^(?:#WEITERLEITUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("lbe", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("li", Pattern.compile("^(?:#DOORVERWIJZING|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ln", Pattern.compile("^(?:#REDIRECTION|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("map-bms", Pattern.compile("^(?:#ALIH|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("mg", Pattern.compile("^(?:#REDIRECTION|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("mhr", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("mo", Pattern.compile("^(?:#REDIRECTEAZA|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("myv", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("mzn", Pattern.compile("^(?:#تغییرمسیر|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("nah", Pattern.compile("^(?:#REDIRECCIÓN|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("os", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("pdc", Pattern.compile("^(?:#WEITERLEITUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("qu", Pattern.compile("^(?:#REDIRECCIÓN|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("rmy", Pattern.compile("^(?:#REDIRECTEAZA|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ruq", Pattern.compile("^(?:#REDIRECTEAZA|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ruq-cyrl", Pattern.compile("^(?:#пренасочување|#види|#Пренасочување|#ПРЕНАСОЧУВАЊЕ|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ruq-grek", Pattern.compile("^(?:#ΑΝΑΚΑΤΕΥΘΥΝΣΗ|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ruq-latn", Pattern.compile("^(?:#REDIRECTEAZA|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("sah", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("shi", Pattern.compile("^(?:#تحويل|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("simple", Pattern.compile("^(?:#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("sr", Pattern.compile("^(?:#Преусмери|#redirect|#преусмери|#ПРЕУСМЕРИ) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("stq", Pattern.compile("^(?:#WEITERLEITUNG|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("su", Pattern.compile("^(?:#ALIH|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("szl", Pattern.compile("^(?:#PATRZ|#PRZEKIERUJ|#TAM|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("tt", Pattern.compile("^(?:#yünältü|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("tt-cyrl", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ty", Pattern.compile("^(?:#REDIRECTION|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("udm", Pattern.compile("^(?:#перенаправление|#перенапр|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("vep", Pattern.compile("^(?:#suuna|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("vls", Pattern.compile("^(?:#DOORVERWIJZING|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("wa", Pattern.compile("^(?:#REDIRECTION|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("wo", Pattern.compile("^(?:#REDIRECTION|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("xmf", Pattern.compile("^(?:#REDIRECT|#გადამისამართება) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("ydd", Pattern.compile("^(?:#ווייטערפירן|#הפניה|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
+        m.put("zea", Pattern.compile("^(?:#DOORVERWIJZING|#REDIRECT) \\[\\[([^\\]]*)\\]\\]"));
         return m;
     }
 
@@ -100,7 +242,7 @@ public class AnnotatingMarkupParser implements ITextConverter {
                         languageCode)) {
             @Override
             public String getRawWikiContent(String namespace,
-                    String articleName, Map<String, String> templateParameters) {
+                                            String articleName, Map<String, String> templateParameters) {
                 // disable template support
                 // TODO: we need to read template support at least for dates
                 return "";
@@ -112,7 +254,7 @@ public class AnnotatingMarkupParser implements ITextConverter {
      * Convert WikiMarkup to a simple text representation suitable for NLP
      * analysis. The links encountered during the extraction are then available
      * by calling {@code #getWikiLinks()}.
-     * 
+     *
      * @param rawWikiMarkup
      * @return the simple text without the markup
      */
@@ -141,7 +283,7 @@ public class AnnotatingMarkupParser implements ITextConverter {
     }
 
     public void nodesToText(List<? extends Object> nodes, Appendable buffer,
-            IWikiModel model) throws IOException {
+                            IWikiModel model) throws IOException {
         CountingAppendable countingBuffer;
         if (buffer instanceof CountingAppendable) {
             countingBuffer = (CountingAppendable) buffer;
@@ -245,7 +387,7 @@ public class AnnotatingMarkupParser implements ITextConverter {
     }
 
     public void imageNodeToText(TagNode tagNode, ImageFormat imageFormat,
-            Appendable buffer, IWikiModel model) throws IOException {
+                                Appendable buffer, IWikiModel model) throws IOException {
         nodesToText(tagNode.getChildren(), buffer, model);
     }
 
